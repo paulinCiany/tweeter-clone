66655ac9357aea4a3378c01539c4011223e19d2a
paulinCiany <paulinlubamba24@gmail.com>
2024-01-16 19:32:47 +0200

    error fixed from timeline component

--- src/Component/Trend.jsx
+++ src/Component/Trend.jsx
@@ -1,6 +1,7 @@
+
 function Trend() {
     return (
-        <>
+        <> 
             <div className='flexRow'>
                 <div className='styleInput'>
                     <span >Trending in Turkey</span>
@@ -9,6 +10,35 @@ function Trend() {
                 </div>
                 <img src="src/assets/Icons/More-2.svg" alt="" />
             </div>
+
+            <div className='flexRow'>
+                <div className='styleInput'>
+                  <span >Trending in Turkey</span>
+                  <span className='tagContent'>#SQUID</span>
+                  <span>2,066 Tweets</span>
+                </div>
+                <img src="src/assets/Icons/More-2.svg" alt="" />
+              </div>
+              <div className='flexRow'>
+                <div className='styleInput'>
+                  <span >Trending in Turkey</span>
+                  <span className='tagContent'>#SQUID</span>
+                  <span>2,066 Tweets</span>
+                </div>
+                <img src="src/assets/Icons/More-2.svg" alt="" />
+              </div>
+              <div className='flexRow'>
+                <div className='styleInput'>
+                  <span >Trending in Turkey</span>
+                  <span className='tagContent'>#SQUID</span>
+                  <span>2,066 Tweets</span>
+                </div>
+                <img src="src/assets/Icons/More-2.svg" alt="" />
+              </div>
+
+              <div>
+                <a href="">Show more</a>
+              </div>
 
         </>
     )
